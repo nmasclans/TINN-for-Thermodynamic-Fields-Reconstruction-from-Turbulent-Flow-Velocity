0da8763bedc9b92c2500bfdce2b81db8d8f07104
nmasclans <nuria.masclans@upc.edu>
2022-12-25 11:12:53 +0100

    update bash file BSC with new arguments and HP tunning, including lr scheduler and early stopping

--- execute_in_BSC_trainer.sh
+++ execute_in_BSC_trainer.sh
@@ -2,8 +2,8 @@
 
 ##SBATCH -N 1 
 #SBATCH -c 80
-#SBATCH --gres=gpu:2
-#SBATCH --time=48:00:00
+#SBATCH --gres=gpu:1
+#SBATCH --time=24:00:00
 #SBATCH --output=PINNS_training_%j.out
 ##SBATCH --error=___%j.err
 
@@ -14,21 +14,27 @@ module load tensorflow/2.5.0
 
 
 for HIDDEN_LAYERS in 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20; do
-    for NEURONS_PER_HIDDEN_LAYER in 8 16 32 64 128 256; do
-        for LEARNING_RATE in 0.001 0.0001; do
-            echo ""
-            echo "----------------------------------------------------------------------------------"
-            echo "----------------------------------------------------------------------------------"
-            echo "Hidden Layers: $HIDDEN_LAYERS, Neurons per hidden layer: $NEURONS_PER_HIDDEN_LAYER, Learning rate: $LEARNING_RATE"
-            echo "----------------------------------------------------------------------------------"
-            echo "----------------------------------------------------------------------------------"
-            python3 my_trainer.py \
-		        --num_hidden_layers $HIDDEN_LAYERS \
-		        --num_neurons_per_layer $NEURONS_PER_HIDDEN_LAYER \
-                --learning_rate $LEARNING_RATE \
-	            --num_epochs 30 \
-		        --training_filename "/gpfs/scratch/bsc44/bsc44529/datasets/post_processed/59300000_5features_4targets/3d_high_pressure_turbulent_channel_flow_53900000.npz" \
-		        --validation_filename "/gpfs/scratch/bsc44/bsc44529/datasets/post_processed/59300000_5features_4targets/3d_high_pressure_turbulent_channel_flow_59300000.npz"
-        done
-    done
+	for NEURONS_PER_HIDDEN_LAYER in 8 16 32 64 128; do
+		for BATCH_SIZE in 16 64; do
+			for SEED in 1 2 3 4 5; do
+				echo ""
+				echo "----------------------------------------------------------------------------------"
+				echo "----------------------------------------------------------------------------------"
+				echo "Hidden Layers: $HIDDEN_LAYERS, Neurons per hidden layer: $NEURONS_PER_HIDDEN_LAYER, Seed: $SEED"
+				echo "----------------------------------------------------------------------------------"
+				echo "----------------------------------------------------------------------------------"
+				python3 my_trainer.py \
+					--num_hidden_layers $HIDDEN_LAYERS \
+					--num_neurons_per_layer $NEURONS_PER_HIDDEN_LAYER \
+					--learning_rate 0.001 \
+					--lr_decay "exp" \
+					--seed $SEED \
+					--batch_size $BATCH_SIZE \
+					--num_epochs 30 \
+					--loss "Supervised_PINNS" \
+					--training_filename "/gpfs/scratch/bsc44/bsc44529/datasets/post_processed/59300000_5features_4targets/3d_high_pressure_turbulent_channel_flow_53900000.npz" \
+					--validation_filename "/gpfs/scratch/bsc44/bsc44529/datasets/post_processed/59300000_5features_4targets/3d_high_pressure_turbulent_channel_flow_59300000.npz"
+			done
+		done
+	done
 done
