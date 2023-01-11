41643bf938fdda78a3083b09d900727133b893b3
nmasclans <nuria.masclans@upc.edu>
2023-01-11 16:48:12 +0100

    major modifications for making and ploting classification from predicted regression values

--- my_utils.py
+++ my_utils.py
@@ -1,6 +1,51 @@
 import tensorflow as tf
+import numpy as np
+
 from datetime import datetime
 
 def tf_print_time():
     now = datetime.now().strftime("%H:%M:%S")
     tf.print("Current Time:",now)
+
+def transform_targets_to_original_scaling(args, *multiple_targets):
+    """
+    Converts input tensors from scaled range to original range
+    Args:
+        args: my_parser arguments
+        multiple_targets (iteable of tf.tensor): tf tensor, of shape = (batch_size,num_targets)
+    Returns:
+        tuple of np arrays of targets rescaled to the original scaling
+    """
+    def _targets_to_original_scaling(args, targets):
+        targets_transformed = np.zeros_like(targets)
+        for targ_idx in range(args.num_targets):
+            targ_name = args.targets_name[targ_idx]
+            targ_min  = args.targets_limits[targ_name][0]
+            targ_max  = args.targets_limits[targ_name][1]
+            assert (targ_max-targ_min) > 0
+            targets_transformed[:,targ_idx] = (targets[:,targ_idx] - args.min_value) * (targ_max-targ_min) / (args.max_value-args.min_value) + targ_min 
+        return targets_transformed
+
+    return (_targets_to_original_scaling(args, t) for t in multiple_targets)
+
+def classify_state_from_temperature(args, *multiple_T_tensors):
+    """
+    Transforms temperature data (continuous data) to fluid state (discrete, 3 classes) 
+    Args:
+        args: my_parser arguments
+        multiple_T_tensors (iteable of tf.tensor): tf.tensor / np.array of temperature values, shape = (batch_size,)
+    return:
+        tuple of np arrays of fluid states information, shape = (batch_size,), where:
+            0 : liquid-like       (T < args.T_minus)
+            1 : two-phases-like   (args.T_minus < T < args.T_plus)
+            2 : gas-like          (args.T_plus < T)
+    """
+    def _state_from_temperature(args, T):
+        T_minus = args.T_minus
+        T_plus  = args.T_plus
+        state = np.ones(len(T),dtype=int)
+        state[T < T_minus] = 0
+        state[T > T_plus]  = 2
+        return state
+
+    return (_state_from_temperature(args, T_tensor) for T_tensor in multiple_T_tensors)
--- my_visualizers.py
+++ my_visualizers.py
@@ -3,12 +3,15 @@ from matplotlib import pyplot as plt
 
 import tensorflow as tf
 
+from my_utils import transform_targets_to_original_scaling, classify_state_from_temperature
+
 # Results Visualizaton
 
 # Plot the quantities of the validation dataset, single validation batch
 # Plot scatterplot + histogram
 
 def visualize_prediction(y_gt, y_pred, epoch, batch, args):
+    (y_gt, y_pred) = transform_targets_to_original_scaling(args, y_gt, y_pred)
     for target_idx in range(args.num_targets):
         y_gt_0   = y_gt[:,target_idx]
         y_pred_0 = y_pred[:,target_idx]
@@ -34,9 +37,10 @@ def visualize_prediction(y_gt, y_pred, epoch, batch, args):
         plt.close()
         # print(f"Visualization of validation results in '{fig_title}'")
 
-def visualize_prediction_by_xyplanes(y_gt, y_pred, epoch, batch, args):
+def visualize_prediction_regression_by_xyplanes(y_gt, y_pred, epoch, batch, args):
     assert y_gt.ndim == 2 and y_pred.ndim == 2
     assert y_gt.shape[0] == 128**3 and y_pred.shape[0] == 128**3, "--batch_size of prediction must be set to 128**3"
+    (y_gt, y_pred) = transform_targets_to_original_scaling(args, y_gt, y_pred)
     for target_idx in range(args.num_targets):
         y_gt_target   = y_gt[:,target_idx]
         y_pred_target = y_pred[:,target_idx]
@@ -44,16 +48,46 @@ def visualize_prediction_by_xyplanes(y_gt, y_pred, epoch, batch, args):
         # recover spatial dimension
         y_gt_target_d   = tf.reshape(y_gt_target,   args.spatial_dimension)         # shape: [128,128,128]
         y_pred_target_d = tf.reshape(y_pred_target, args.spatial_dimension)
-        # plot contourf of middle plane z
+        # plot imshow of middle plane z
         # ground truth
         fig_title = f"figures/{target_name}_contourf_E{epoch}_B{batch}_gt.png"
         plt.figure()
         plt.imshow(y_gt_target_d[64,:,:]); plt.axis('scaled'); plt.colorbar()
+        plt.gca().invert_yaxis()
         plt.savefig(fig_title)
         plt.close()
         # prediction
         fig_title = f"figures/{target_name}_contourf_E{epoch}_B{batch}_pred.png"
         plt.figure()
         plt.imshow(y_pred_target_d[64,:,:]); plt.axis('scaled'); plt.colorbar()
+        plt.gca().invert_yaxis()
         plt.savefig(fig_title)
         plt.close()
+
+def visualize_prediction_classification_by_xyplanes(y_gt, y_pred, epoch, batch, args):
+    assert y_gt.ndim == 2 and y_pred.ndim == 2
+    assert y_gt.shape[0] == 128**3 and y_pred.shape[0] == 128**3, "--batch_size of prediction must be set to 128**3"
+    assert args.targets_name[2] == 'T', "'visualize_prediction_classification_by_xyplanes' function needs --target_name 3rd element to be 'T'"
+    (y_gt, y_pred) = transform_targets_to_original_scaling(args, y_gt, y_pred)
+    T_gt   = y_gt[:,2]
+    T_pred = y_pred[:,2]
+    # predict class from regression data!
+    (state_gt, state_pred) = classify_state_from_temperature(args, T_gt, T_pred)
+    # recover spatial dimension
+    state_gt_d   = tf.reshape(state_gt,   args.spatial_dimension)   # shape: [128,128,128]
+    state_pred_d = tf.reshape(state_pred, args.spatial_dimension)
+    # plot contourf of middle plane z
+    # ground truth
+    fig_title = f"figures/fluid_state_contourf_E{epoch}_B{batch}_gt.png"
+    plt.figure()
+    plt.imshow(state_gt_d[64,:,:]); plt.axis('scaled'); plt.colorbar()
+    plt.gca().invert_yaxis()
+    plt.savefig(fig_title)
+    plt.close()
+    # prediction
+    fig_title = f"figures/fluid_state_contourf_E{epoch}_B{batch}_pred.png"
+    plt.figure()
+    plt.imshow(state_pred_d[64,:,:]); plt.axis('scaled'); plt.colorbar()
+    plt.gca().invert_yaxis()
+    plt.savefig(fig_title)
+    plt.close()
